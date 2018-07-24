e8d23463cf72233a6e286db5ca8306a2d698c69b
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-07-24 09:51:32 +0100

    removed comments

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,22 @@
-
 module.exports = {
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-  // A set of global variables that need to be available in all test environments
   globals: {
     "ts-jest": {
       "tsConfigFile": "tsconfig.json"
     }
   },
-  // An array of file extensions your modules use
   moduleFileExtensions: [
     "ts",
     "tsx",
     "js"
   ],
-  // The test environment that will be used for testing
   testEnvironment: "node",
-  // The glob patterns Jest uses to detect test files
   testMatch: [
     "**/test/**/*.test.(ts|tsx|js)"
   ],
-  // A map from regular expressions to paths to transformers
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
-  // Indicates whether each individual test should be reported during the run
   verbose: true
 };
